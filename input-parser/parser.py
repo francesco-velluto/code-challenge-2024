a4260583e99c765e9b5e53445d12d65de1371cc1
Francesco Velluto <f.velluto@reply.it>
2024-03-21 17:07:21 +0100

    parser

--- input-parser/parser.py
+++ input-parser/parser.py
@@ -1,7 +1,36 @@
 import ply.lex as lex
 
-# List of token names.   This is always required
 tokens = (
    'NUMBER',
    'LETTER'
 )
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+def t_LETTER(t):
+    r'[A-Z]'
+    return t
+
+
+def t_ws(self, t):
+    r'\ |\t'
+    pass
+
+def t_error(self, t):
+    r'.'
+    print("Character not recognized '%s'", t.value)
+    return t
+    
+def input(self, s):
+    self.lexer.input(s)
+    
+def token(self):
+    tok = self.lexer.token()
+    if tok is None:
+        print("Emitted token 'EOF'")
+    else:
+        print("Emitted token '%s'", tok.type)
+    return tok
